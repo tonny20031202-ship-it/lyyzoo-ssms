02729297b098c6098f6a1c0722ae48fd33320244
1at <13701826712@163.com>
2026-05-14 02:31:25 +0000

    5-3

--- src/com/lizhou/filter/VisitFilter.java
+++ src/com/lizhou/filter/VisitFilter.java
@@ -1,6 +1,9 @@
 package com.lizhou.filter;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -13,37 +16,41 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.lizhou.bean.User;
 
-/**
- * 如果用户没有登录，返回登录界面
- * @author bojiangzhou
- *
- */
 public class VisitFilter implements Filter {
 
+	private static final Set<String> WHITE_LIST = new HashSet<>(Arrays.asList(
+			"index.jsp",
+			"404.jsp",
+			"500.jsp",
+			"login.jsp",
+			"error.jsp"
+	));
+
 	public void destroy() {
-		
+
 	}
 
 	public void doFilter(ServletRequest req, ServletResponse rep, FilterChain chain) throws IOException, ServletException {
 		HttpServletRequest request = (HttpServletRequest) req;
 		HttpServletResponse response = (HttpServletResponse) rep;
-		
+
 		User user = (User) request.getSession().getAttribute("user");
-		
+
 		String contextPath = request.getContextPath();
-		
 		String uri = request.getRequestURI();
-		uri = uri.substring(uri.lastIndexOf("/")+1, uri.length());
-		
-		if(user != null){
+
+		int lastSlashIndex = uri.lastIndexOf("/");
+		String resource = lastSlashIndex >= 0 ? uri.substring(lastSlashIndex + 1) : uri;
+
+		if (user != null || WHITE_LIST.contains(resource)) {
 			chain.doFilter(request, response);
-		} else{
-			response.sendRedirect(contextPath+"/index.jsp");
+		} else {
+			response.sendRedirect(contextPath + "/index.jsp");
 		}
 	}
 
 	public void init(FilterConfig arg0) throws ServletException {
-		
+
 	}
 
 }
